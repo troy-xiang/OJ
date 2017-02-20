f0d434c8ba005b7ff8f4b8b7996eaacaa51b5400
Troy <troy@TroydeMacBook-Pro.local>
2017-02-20 23:27:49 +0800

    Troy push 20170220 to github/OJ/leetcode

--- leetcode/src/practise20170220/Solution167.java
+++ leetcode/src/practise20170220/Solution167.java
@@ -12,16 +12,19 @@ import java.util.Arrays;
  */
 public class Solution167 {
     public int[] twoSum(int[] numbers, int target) {
-        if(numbers.length < 2) {
+        if(numbers == null || numbers.length < 2) {
             return new int[] {};
         }
 
-        int length = numbers.length;
-        for(int i = 0; i < length; i++) {
-            for(int j = i + 1; j < length; j++) {
-                if(numbers[i] + numbers[j] == target) {
-                    return new int[] {i + 1, j + 1};
-                }
+        int left = 0, right = numbers.length - 1;
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if(sum < target) {
+                left++;
+            } else{
+                right--;
             }
         }
         return new int[] {};
---
+++ leetcode/src/practise20170220/Solution455.java
@@ -0,0 +1,28 @@
+package practise20170220;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by troy on 2017/2/20.
+ * 455. Assign Cookies
+ * Assume you are an awesome parent and want to give your children some cookies.
+ * But, you should give each child at most one cookie. Each child i has a greed factor gi,
+ * which is the minimum size of a cookie that the child will be content with;
+ * and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i,
+ * and the child i will be content.
+ * Your goal is to maximize the number of your content children and output the maximum number.
+ */
+public class Solution455 {
+    public int findContentChildren(int[]g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        for(int i = 0; i < s.length && count < g.length; i++) {
+            if(g[count] <= s[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
